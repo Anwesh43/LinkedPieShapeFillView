1b6503048f201042aeefb1a32449e3569fdbcd65
Anwesh43 <anweshthecool0@gmail.com>
2020-06-12 16:28:28 +0530

    adding PieShapeFillView class

--- pieshapefillview/src/main/java/com/anwesh/uiprojects/pieshapefillview/PieShapeFillView.kt
+++ pieshapefillview/src/main/java/com/anwesh/uiprojects/pieshapefillview/PieShapeFillView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawPSFNode(i : Int, scale : Float, paint : Paint) {
     drawPieShapeFill(scale, size, paint)
     restore()
 }
+
+class PieShapeFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
