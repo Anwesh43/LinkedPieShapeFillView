35763a1ba2497de4939ce4ebba795cef086a3ec9
Anwesh43 <anweshthecool0@gmail.com>
2020-06-12 13:38:06 +0530

    adding constants

--- pieshapefillview/src/main/java/com/anwesh/uiprojects/pieshapefillview/PieShapeFillView.kt
+++ pieshapefillview/src/main/java/com/anwesh/uiprojects/pieshapefillview/PieShapeFillView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.pieshapefillview
 /**
  * Created by anweshmishra on 12/06/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val colors : Array<String> = arrayOf("#4CAF50", "#FF5722", "#3F51B5", "#009688", "#9C27B0")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 15f
